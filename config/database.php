1dae6eca5cecab8a7b192d1d147c37fe306ca312
Juan <juan.ochoad@upb.edu.co>
2021-05-24 09:01:43 -0500

    added aws database

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'sqlsrv'),
 
     /*
     |--------------------------------------------------------------------------
@@ -81,11 +81,11 @@ return [
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '34.234.201.152'),
             'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'GameSolutions'),
+            'username' => env('DB_USERNAME', 'SA'),
+            'password' => env('DB_PASSWORD', 'contraseniA1'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
